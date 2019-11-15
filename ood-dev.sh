d9849552f7ea28d55f7562d00fd43d145289c53b
Jeff Ohrstrom <johrstrom@hotmail.com>
2019-11-15 11:35:20 -0500

    add scl to ood-dev

--- ood-dev.sh
+++ ood-dev.sh
@@ -2,8 +2,8 @@
 
 ctr=$(buildah from centos:7) 
 
-dev_packages="lsof sudo sqlite-devel gcc rh-ruby24-ruby-devel zlib-devel git curl make wget"
-dev_packages="${dev_packages} rh-nodejs6 rh-ruby24 rpm-build"
+dev_packages="lsof sudo sqlite-devel gcc rh-ruby25-ruby-devel zlib-devel git curl make wget"
+dev_packages="${dev_packages} rh-nodejs10 rh-ruby25 rpm-build gcc-c++"
 
 buildah run $ctr -- yum install -y centos-release-scl
 buildah run $ctr -- yum-config-manager --enable rhel-server-rhscl-7-rpms
@@ -16,6 +16,12 @@ buildah run $ctr -- useradd -u $(id -u) --create-home --gid $USER $USER
 buildah run $ctr -- usermod -a -G wheel $USER
 buildah run $ctr -- passwd --delete $USER
 buildah run $ctr -- passwd --delete root
+
+# scl hack
+buildah run $ctr -- mkdir -p /opt/ood/ondemand/
+buildah run $ctr -- touch /opt/ood/ondemand/enable
+buildah run $ctr -- mkdir -p /etc/scl/conf/
+buildah run $ctr bash -c 'echo "/opt/ood" > /etc/scl/conf/ondemand'
 
 buildah commit $ctr ood-dev:latest
 buildah rm $ctr
