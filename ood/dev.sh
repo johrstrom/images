83470ba51a41e5cc03f770812d370f6394a4b6d3
Jeff Ohrstrom <johrstrom@hotmail.com>
2020-01-30 10:06:16 -0500

    make ood/dev be bash instead of strings. also trying to build containers in ood:dev

--- ood/dev.sh
+++ ood/dev.sh
@@ -2,13 +2,13 @@
 
 ctr=$(buildah from centos:7) 
 
-dev_packages="lsof sudo sqlite-devel gcc rh-ruby25-ruby-devel zlib-devel git curl make wget"
-dev_packages="${dev_packages} rh-nodejs10 rh-ruby25 rpm-build gcc-c++"
+dev_packages=(lsof sudo sqlite-devel gcc rh-ruby25-ruby-devel zlib-devel git curl make wget)
+dev_packages+=(rh-nodejs10 rh-ruby25 rpm-build gcc-c++ libseccomp vim-enhanced)
 
 buildah run $ctr -- yum install -y centos-release-scl
 buildah run $ctr -- yum-config-manager --enable rhel-server-rhscl-7-rpms
 
-buildah run $ctr -- yum install -y ${dev_packages}
+buildah run $ctr -- yum install -y ${dev_packages[@]}
 buildah run $ctr -- yum clean all
 
 buildah run $ctr -- groupadd $USER
@@ -22,6 +22,9 @@ buildah run $ctr -- mkdir -p /opt/ood/ondemand/
 buildah run $ctr -- touch /opt/ood/ondemand/enable
 buildah run $ctr -- mkdir -p /etc/scl/conf/
 buildah run $ctr bash -c 'echo "/opt/ood" > /etc/scl/conf/ondemand'
+
+#
+buildah run $ctr -- ln -s /usr/lib64/libdevmapper.so.1.02 /usr/lib64/libdevmapper.so.1.02.1
 
 buildah commit $ctr ood:dev
 buildah rm $ctr
